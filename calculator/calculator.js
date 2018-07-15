4a6c345ce01a03489de80f3571aa6fad8b6a905a
Tom Smith <TommyTriesTech@gmail.com>
2018-07-15 09:36:58 +0100

    Completed calculator exercises (no comments)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
-	
+function add (a, b) {
+	return a+b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum (list) {
+	const adder = (value, currentValue) => value + currentValue;
+	if (list.length === 0){
+		return 0;
+	} else {
+		return list.reduce(adder);
+	}
 }
 
-function multiply () {
-	
+function multiply (a) {
+	const adder = (value, currentValue) => value * currentValue;
+	return a.reduce(adder);
 }
 
-function power() {
-	
+function power(a, b) {
+	var list = [];
+	for (var i = 0; i < b; i++){
+		list.push(a);
+	}
+	const adder = (value, currentValue) => value * currentValue;
+	return list.reduce(adder);
 }
 
-function factorial() {
-	
+function factorial(x) {
+	if (x === 0){
+		return 1
+	} else {
+	var list = [...Array(x + 1).keys()];
+	list.shift();
+	const adder = (value, currentValue) => value * currentValue;
+	return list.reduce(adder);
+	}
+
+// for (var i = 0; i < x; i++){
+// 	x
+// }
 }
 
 module.exports = {
